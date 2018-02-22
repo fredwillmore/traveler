a003608f3334ceaf4f285465728dada10fa0605f
Fred Willmore <fredwillmore@gmail.com>
2018-02-21 21:25:03 -0800

    added authentication tokens

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161201112127) do
+ActiveRecord::Schema.define(version: 20161203112013) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,15 @@ ActiveRecord::Schema.define(version: 20161201112127) do
     t.string   "name",       limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "authentication_tokens", force: :cascade do |t|
+    t.string   "token"
+    t.integer  "user_id"
+    t.datetime "expires_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_authentication_tokens_on_user_id", using: :btree
   end
 
   create_table "challenge_translations", force: :cascade do |t|
@@ -219,4 +228,5 @@ ActiveRecord::Schema.define(version: 20161201112127) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "authentication_tokens", "users"
 end
