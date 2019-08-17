c2963336e01a6705ea35bc9f748ef25046559640
Fred Willmore <fredwillmore@gmail.com>
2019-08-16 22:51:52 -0700

    just get these merged

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,5 +5,5 @@ describe User do
   it { should validate_presence_of :first_name }
   it { should validate_presence_of :last_name }
   it { should validate_presence_of :email }
-  it { should validate_uniqueness_of :email }
+  it { should validate_uniqueness_of(:email).ignoring_case_sensitivity }
 end
