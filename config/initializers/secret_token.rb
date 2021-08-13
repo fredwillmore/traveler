92abe67bde4af3aef98cbfc44ce295d09ca36dae
Fred Willmore <fredwillmore@gmail.com>
2021-08-12 22:16:50 -0700

    updated deprecated reference

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Traveler::Application.config.secret_token = 'd3da88043ea836744b1dac4abf200372a20ce0334f5ac8da18138f33016b0c0c4bf2347ab78a7c8f70a77941ecd6dd90cef8745d03ba2b337384a84013cd452d'
+Traveler::Application.config.secret_key_base = 'd3da88043ea836744b1dac4abf200372a20ce0334f5ac8da18138f33016b0c0c4bf2347ab78a7c8f70a77941ecd6dd90cef8745d03ba2b337384a84013cd452d'
