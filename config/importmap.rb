fdd643d853d801a45d63bba98e637c755d3f3c10
Fred Willmore <fredwillmore@gmail.com>
2023-01-07 17:50:27 -0800

    copied config settings over from newly created project that doesn't have the issue with import statement outside a module

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,4 +1,11 @@
 # Pin npm packages by running ./bin/importmap
 
-pin "application", preload: true
+pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
+pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+
 pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.6.3/dist/jquery.js"
+pin "vue", to: "https://ga.jspm.io/npm:vue@3.2.26/dist/vue.esm-browser.js"
+pin_all_from "app/javascript/controllers", under: "controllers"
+
+pin "application", preload: true
