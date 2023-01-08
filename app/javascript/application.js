0b0e3fdf17e409400bfb768880c1f0149f8efdb8
Fred Willmore <fredwillmore@gmail.com>
2023-01-07 17:52:04 -0800

    trying to add import statements in application.js, debugging SyntaxError: Cannot use import statement outside a module

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,1 +1,9 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+// why do this work
+//= require jquery
+//= require jquery-ui
+//= require home
+
+// but not this
+import "@hotwired/turbo-rails"
+// import "controllers"
