b6fb576b51fe865a06c88a72039787d65a0f5b41
Fred Willmore <fredwillmore@gmail.com>
2023-01-08 21:44:42 -0800

    temporarily remove migration to create active storage table

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,9 +1,3 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-// why do this work
-//= require jquery
-//= require jquery-ui
-//= require home
-
-// but not this
+import "controllers"
 import "@hotwired/turbo-rails"
-// import "controllers"
