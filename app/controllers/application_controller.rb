d225d2f0173d05ef78dbe2a527a45a3e3a26f75a
Fred Willmore <fredwillmore@gmail.com>
2023-01-15 10:01:00 -0800

    temporarily disable authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   before_action :set_locale
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
 
   protected
   def authenticate_user!
