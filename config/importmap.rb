65d7e0ac45398d765e4a3fb519332be88b951b38
Fred Willmore <fredwillmore@gmail.com>
2023-01-15 08:11:30 -0800

    just pin jquery and application

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,13 +1,4 @@
 # Pin npm packages by running ./bin/importmap
 
-pin "@hotwired/turbo-rails", to: "https://ga.jspm.io/npm:@hotwired/turbo-rails@7.2.4/app/javascript/turbo/index.js"
-pin "@hotwired/stimulus", to: "https://ga.jspm.io/npm:@hotwired/stimulus@3.2.1/dist/stimulus.js"
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
-
-pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.6.3/dist/jquery.js"
-pin "vue", to: "https://ga.jspm.io/npm:vue@3.2.26/dist/vue.esm-browser.js"
-pin_all_from "app/javascript/controllers", under: "controllers"
-
 pin "application", preload: true
-pin "@hotwired/turbo", to: "https://ga.jspm.io/npm:@hotwired/turbo@7.2.4/dist/turbo.es2017-esm.js"
-pin "@rails/actioncable/src", to: "https://ga.jspm.io/npm:@rails/actioncable@7.0.4/src/index.js"
+pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.6.3/dist/jquery.js"
