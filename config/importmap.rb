dde85aa8dec1d281e1ab150037524f37f366cdd5
Fred Willmore <fredwillmore@gmail.com>
2023-02-04 13:34:36 -0800

    pinned vue and axios packages

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,4 +1,12 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application", preload: true
-pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.6.3/dist/jquery.js"
+
+pin_all_from "app/javascript/controllers", under: "controllers"
+
+pin "vue", to: "https://ga.jspm.io/npm:vue@3.2.29/dist/vue.esm-browser.js"
+
+pin "axios", to: "https://cdn.skypack.dev/axios@1.3.2"
+pin "#lib/adapters/http.js", to: "https://ga.jspm.io/npm:@jspm/core@2.0.0/nodelibs/@empty.js"
+pin "#lib/platform/node/index.js", to: "https://ga.jspm.io/npm:@jspm/core@2.0.0/nodelibs/@empty.js"
+pin "#lib/platform/node/classes/FormData.js", to: "https://ga.jspm.io/npm:@jspm/core@2.0.0/nodelibs/@empty.js"
