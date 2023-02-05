b99d3944e5b01e8693019b31ba38be842f4e60c6
Fred Willmore <fredwillmore@gmail.com>
2023-02-04 16:46:04 -0800

    keeping old JS files for now

---
+++ app/javascript/temp/places.js
@@ -0,0 +1,9 @@
+// Place all the behaviors and hooks related to the matching controller here.
+// All this logic will automatically be available in application.js.
+
+(function() {
+  $("#place_info_close").click(function() {
+    return $("#place_info").hide();
+  });
+
+}).call(this);
