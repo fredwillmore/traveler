a94e1a25014b90c88562ae0506261dc79e9d853a
Fred Willmore <fredwillmore@gmail.com>
2023-03-03 21:33:22 -0800

    updated some model specs

--- spec/models/place_spec.rb
+++ spec/models/place_spec.rb
@@ -3,4 +3,17 @@ require 'rails_helper'
 describe Place do
   it { should belong_to :location }
   it { should have_and_belong_to_many :place_types }
+
+  describe "class methods" do
+    subject { described_class }
+
+    describe "find_or_create_by_external_id" do
+      # let(:external_id) { "abcdefghijklmnop-0123456789" }
+      let(:external_id) { "ChIJV5D16suglVQR-kJRdSOrUwo" }
+
+      it "works right" do
+        expect { subject.find_or_create_by_external_id(external_id) }.not_to raise_error
+      end
+    end
+  end
 end
--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -8,22 +8,36 @@ describe Player do
   it { should have_one_attached(:avatar) }
 
   let(:player) { create(:player) }
-
+  let(:test_file_path) { Rails.application.root + "spec/fixtures/files/test_image.jpg" }
+  let(:test_file) { File.open(test_file_path) }
+  let(:test_file_name) { File.basename(test_file_path) }
+  let(:io) { test_file.to_io }
+  let(:avatar) do
+    ActiveStorage::Blob.create_and_upload!(
+      io: io,
+      filename: test_file_name
+    )
+  end
+  
   describe 'avatar' do
-    let(:test_file_path) { Rails.application.root + "spec/fixtures/files/test_image.jpg" }
-    let(:test_file) { File.open(test_file_path) }
-    let(:test_file_name) { File.basename(test_file_path) }
-    let(:io) { test_file.to_io }
-    let(:avatar) do
-      ActiveStorage::Blob.create_and_upload!(
-        io: io,
-        filename: test_file_name
-      )
-    end
-    
     it 'works right' do
       expect { player.update(avatar: avatar) }.not_to raise_error
       expect(player.avatar.attachment.blob).to eq avatar
+    end
+  end
+  
+  describe 'avatar_urls' do
+    let(:player) { create(:player, avatar: avatar) }
+    
+    it 'works right' do
+      expect(player.avatar_urls.keys).to include(
+        :xsmall,
+        :small,
+        :medium,
+        :large,
+        :xlarge,
+        :xxlarge
+      )
     end
   end
 end
