495622acfdd02a6086e0a8661df601b0ed2a546c
Fred Willmore <fredwillmore@gmail.com>
2023-03-18 09:11:56 -0700

    moved old-style JS and templates into vue component

--- app/javascript/controllers/App.js
+++ app/javascript/controllers/App.js
@@ -40,6 +40,69 @@ export default {
           </ul>
         </div>
       </div>
+      <div id="place_info" v-if="showPlaceInfo">
+        <div id="place_info_handle" class="handle">
+          <a v-on:click="hidePlaceInfo" href="#">[ x ]</a>
+          Place Info
+          <div id="place_info_content">
+            <table>
+              <tr>
+                <td>ID:</td>
+                <td>{{placeInfo.id}}</td>
+              </tr>
+              <tr>
+                <td>Name:</td>
+                <td>{{placeInfo.name}}</td>
+              </tr>
+              <tr>
+                <td>Rating:</td>
+                <td>{{placeInfo.rating}}</td>
+              </tr>
+              <tr>
+                <td>Travel Time Walking:</td>
+                <td>{{placeInfo.travelTimeWalking}}</td>
+              </tr>
+              <tr>
+                <td>Travel Time Bicycling:</td>
+                <td>{{placeInfo.travelTimeBicycling}}</td>
+              </tr>
+              <tr>
+                <td>Travel Time Driving:</td>
+                <td>{{placeInfo.travelTimeDriving}}</td>
+              </tr>
+              <tr>
+                <td>Travel Time Transit:</td>
+                <td>{{placeInfo.travelTimeTransit}}</td>
+              </tr>
+              <tr>
+                <td>Food Value/Food Cost:</td>
+                <td>
+                  {{placeInfo.foodValue}} / {{placeInfo.foodCost}}
+                </td>
+              </tr>
+              <tr>
+                <td>Drink Value/Drink Cost:</td>
+                <td>
+                  {{placeInfo.drinkValue}} / {{placeInfo.drinkCost}}
+                </td>
+              </tr>
+              <tr>
+                <td></td>
+                <td>
+                  <input id="place_info_go_walk" type="button" value="place_info.go_walk" />
+                </td>
+                <!-- allow player to have/rent a car - then add driving option -->
+              </tr>
+              <tr>
+                <td></td>
+                <td>
+                  <input id="place_info_go_taxi" type="button" value="place_info.go_taxi" />
+                </td>
+              </tr>
+            </table>
+          </div>
+        </div>
+      </div>
     `,
     props: ['lat', 'lng', 'zoom', 'playersUrl'],
     data() {
@@ -50,61 +113,24 @@ export default {
         searchTerm: '',
         searchSuggestions: [],
         searchIsOpen: false,
+        placeReference: null, // I don't necessarily like this
+        showPlaceInfo: false,
+        placeInfo: {
+          id: null,
+          name: null,
+          rating: null,
+          travelTimeWalking: null,
+          travelTimeBicycling: null,
+          travelTimeDriving: null,
+          travelTimeTransit: null,
+          foodValue: null,
+          foodCost: null,
+          drinkValue: null,
+          drinkCost: null,
+        },
       }
     },
     liveMarkers: [],
-    showPlaceInfo: function(e) {
-      axios.request(
-        '/places/' + this.placeReference
-      ).then(
-        (response) => {
-          debugger
-          ajaxPlaceHandler(data)
-        }
-      )
-
-
-      // var distanceMatrixRequest, myActivePlaceMarker, placeDetailsRequest;
-      // myActivePlaceMarker = this;
-
-      // $("#place_info").css('left', mouseX);
-      // $("#place_info").css('top', mouseY);
-      // placeDetailsRequest = {
-      //   reference: this.placeReference
-      // };
-      // $.ajax({
-      //   url: '/places/' + this.placeReference,
-      //   type: "GET",
-      //   data: {
-      //     external_id: this.placeReference
-      //   },
-      //   dataType: 'json',
-      //   success: function(data) {
-      //     return ajaxPlaceHandler(data);
-      //   }
-      // });
-      // placess.getDetails(placeDetailsRequest, handleDetailResult);
-      // travelTimes['WALKING'] = travelTimes['BICYCLING'] = travelTimes['DRIVING'] = travelTimes['TRANSIT'] = 0;
-      // distanceMatrixRequest = {
-      //   origins: [$.myLatLng],
-      //   destinations: [this.position],
-      //   travelMode: google.maps.TravelMode.WALKING
-      // };
-      // distanceMatrix.getDistanceMatrix(distanceMatrixRequest, handleDistanceMatrixWalkingResult);
-      // distanceMatrixRequest = {
-      //   origins: [$.myLatLng],
-      //   destinations: [this.position],
-      //   travelMode: google.maps.TravelMode.BICYCLING
-      // };
-      // distanceMatrix.getDistanceMatrix(distanceMatrixRequest, handleDistanceMatrixBicyclingResult);
-      // distanceMatrixRequest = {
-      //   origins: [$.myLatLng],
-      //   destinations: [this.position],
-      //   travelMode: google.maps.TravelMode.DRIVING
-      // };
-      // distanceMatrix.getDistanceMatrix(distanceMatrixRequest, handleDistanceMatrixDrivingResult);
-      // return $("#place_info").show();
-    },
 
     mounted() {
       this.initializeMap()
@@ -134,6 +160,83 @@ export default {
         //     return $.searchMap();
         //   }
         // });
+      },
+      hidePlaceInfo: function() {
+        this.showPlaceInfo = false
+      },
+      displayPlaceInfo: function(e) {
+        axios.request(
+          '/places/' + this.placeReference
+        ).then(
+          (response) => {
+            // alert('hello')
+            
+            var len, results1;
+            results1 = [];
+            var placeInfo = response.data
+            // for (var k = 0, len = placeInfo.length; k < len; k++) {
+            this.placeInfo = {
+              id: placeInfo['id'],
+              name: placeInfo['name'],
+              rating: placeInfo['rating'],
+              travelTimeWalking: '123 minutes',
+              travelTimeBicycling: '30 minutes',
+              travelTimeDriving: '8 minutes',
+              travelTimeTransit: '15 minutes',
+              foodValue: placeInfo['food_value'],
+              foodCost: placeInfo['food_cost'],
+              drinkValue: placeInfo['drink_value'],
+              drinkCost: placeInfo['drink_cost']
+            }
+            this.showPlaceInfo = true
+            // for (const key in placeInfo) {
+            //   results1.push($("#place_info_" + key).html(placeInfo[key]));
+            // }
+            // return results1;
+          }
+        )
+  
+  
+        // var distanceMatrixRequest, myActivePlaceMarker, placeDetailsRequest;
+        // myActivePlaceMarker = this;
+  
+        // $("#place_info").css('left', mouseX);
+        // $("#place_info").css('top', mouseY);
+        // placeDetailsRequest = {
+        //   reference: this.placeReference
+        // };
+        // $.ajax({
+        //   url: '/places/' + this.placeReference,
+        //   type: "GET",
+        //   data: {
+        //     external_id: this.placeReference
+        //   },
+        //   dataType: 'json',
+        //   success: function(data) {
+        //     return ajaxPlaceHandler(data);
+        //   }
+        // });
+        // placess.getDetails(placeDetailsRequest, handleDetailResult);
+        // travelTimes['WALKING'] = travelTimes['BICYCLING'] = travelTimes['DRIVING'] = travelTimes['TRANSIT'] = 0;
+        // distanceMatrixRequest = {
+        //   origins: [$.myLatLng],
+        //   destinations: [this.position],
+        //   travelMode: google.maps.TravelMode.WALKING
+        // };
+        // distanceMatrix.getDistanceMatrix(distanceMatrixRequest, handleDistanceMatrixWalkingResult);
+        // distanceMatrixRequest = {
+        //   origins: [$.myLatLng],
+        //   destinations: [this.position],
+        //   travelMode: google.maps.TravelMode.BICYCLING
+        // };
+        // distanceMatrix.getDistanceMatrix(distanceMatrixRequest, handleDistanceMatrixBicyclingResult);
+        // distanceMatrixRequest = {
+        //   origins: [$.myLatLng],
+        //   destinations: [this.position],
+        //   travelMode: google.maps.TravelMode.DRIVING
+        // };
+        // distanceMatrix.getDistanceMatrix(distanceMatrixRequest, handleDistanceMatrixDrivingResult);
+        // return $("#place_info").show();
       },
       updateBoardForPlayer(response) {
         var player = response.data.find(function(currentValue, index, arr) { currentValue; return currentValue.is_current_player } )
@@ -169,8 +272,9 @@ export default {
       addPlaceMarker(place) {
         let marker = this.addMarker(place.geometry.location)
         marker.set('placeReference', place.reference);
+        this.placeReference = place.reference; // do we need both of these?
         this.$options.liveMarkers.push(marker);
-        google.maps.event.addListener(marker, 'click', this.$options.showPlaceInfo);
+        google.maps.event.addListener(marker, 'click', this.displayPlaceInfo);
       },
 
 
@@ -201,7 +305,7 @@ export default {
           //   return results1;
           // }
         });
-      }
+      },
     },
     computed: {
       // TODO: I don't think this needs to be a computed method
