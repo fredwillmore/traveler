77aee58db13ba05d3fa08884ba6077777122c582
Fred Willmore <fredwillmore@Freds-MacBook-Pro.local>
2023-09-25 19:18:17 -0700

    ran migrations and updated schemaz

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_26_180602) do
   enable_extension "plpgsql"
 
   create_table "action_types", id: :serial, force: :cascade do |t|
-    t.string "name", limit: 255
+    t.string "name"
     t.datetime "created_at", precision: nil
     t.datetime "updated_at", precision: nil
   end
@@ -57,18 +57,18 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_26_180602) do
     t.index ["user_id"], name: "index_authentication_tokens_on_user_id"
   end
 
-  create_table "challenge_translations", id: :serial, force: :cascade do |t|
+  create_table "challenge_translations", force: :cascade do |t|
     t.integer "challenge_id", null: false
-    t.string "locale", limit: 255, null: false
-    t.datetime "created_at", precision: nil
-    t.datetime "updated_at", precision: nil
+    t.string "locale", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.text "challenge_text"
     t.index ["challenge_id"], name: "index_challenge_translations_on_challenge_id"
     t.index ["locale"], name: "index_challenge_translations_on_locale"
   end
 
   create_table "challenge_types", id: :serial, force: :cascade do |t|
-    t.string "name", limit: 255
+    t.string "name"
     t.datetime "created_at", precision: nil
     t.datetime "updated_at", precision: nil
   end
@@ -87,24 +87,24 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_26_180602) do
     t.datetime "updated_at", precision: nil
   end
 
-  create_table "curriculum_area_translations", id: :serial, force: :cascade do |t|
+  create_table "curriculum_area_translations", force: :cascade do |t|
     t.integer "curriculum_area_id", null: false
-    t.string "locale", limit: 255, null: false
-    t.datetime "created_at", precision: nil
-    t.datetime "updated_at", precision: nil
-    t.string "title", limit: 255
+    t.string "locale", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "title"
     t.index ["curriculum_area_id"], name: "index_curriculum_area_translations_on_curriculum_area_id"
     t.index ["locale"], name: "index_curriculum_area_translations_on_locale"
   end
 
   create_table "curriculum_areas", id: :serial, force: :cascade do |t|
-    t.string "title", limit: 255
+    t.string "title"
     t.datetime "created_at", precision: nil
     t.datetime "updated_at", precision: nil
   end
 
   create_table "item_types", id: :serial, force: :cascade do |t|
-    t.string "name", limit: 255
+    t.string "name"
     t.datetime "created_at", precision: nil
     t.datetime "updated_at", precision: nil
   end
@@ -119,31 +119,32 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_26_180602) do
   create_table "locations", id: :serial, force: :cascade do |t|
     t.decimal "lat", precision: 10, scale: 7
     t.decimal "lng", precision: 10, scale: 7
+    t.string "external_id"
     t.datetime "created_at", precision: nil
     t.datetime "updated_at", precision: nil
   end
 
   create_table "place_type_data", id: :serial, force: :cascade do |t|
     t.integer "place_type_id"
-    t.string "data_type", limit: 255
+    t.string "data_type"
     t.float "score"
     t.float "cost"
     t.float "cost_multiplier"
   end
 
-  create_table "place_type_translations", id: :serial, force: :cascade do |t|
+  create_table "place_type_translations", force: :cascade do |t|
     t.integer "place_type_id", null: false
-    t.string "locale", limit: 255, null: false
-    t.datetime "created_at", precision: nil
-    t.datetime "updated_at", precision: nil
-    t.string "label", limit: 255
+    t.string "locale", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "label"
     t.index ["locale"], name: "index_place_type_translations_on_locale"
     t.index ["place_type_id"], name: "index_place_type_translations_on_place_type_id"
   end
 
   create_table "place_types", id: :serial, force: :cascade do |t|
-    t.string "name", limit: 255
-    t.string "label", limit: 255
+    t.string "name"
+    t.string "label"
     t.datetime "created_at", precision: nil
     t.datetime "updated_at", precision: nil
   end
@@ -154,8 +155,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_26_180602) do
   end
 
   create_table "places", id: :serial, force: :cascade do |t|
-    t.string "name", limit: 255
-    t.string "external_id", limit: 255
+    t.string "name"
+    t.string "external_id"
     t.integer "location_id"
     t.float "rating"
     t.datetime "created_at", precision: nil
@@ -171,38 +172,34 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_26_180602) do
 
   create_table "players", id: :serial, force: :cascade do |t|
     t.integer "user_id"
-    t.string "state", limit: 255, default: "normal", null: false
+    t.string "state", default: "normal", null: false
     t.integer "location_id"
     t.integer "destination_id"
     t.integer "level"
-    t.string "name", limit: 255
-    t.string "gender", limit: 255
+    t.string "name"
+    t.string "gender"
     t.datetime "dob", precision: nil
-    t.string "occupation", limit: 255
+    t.string "occupation"
     t.integer "attractiveness"
     t.integer "charisma"
     t.integer "status"
     t.integer "intelligence"
     t.integer "luck"
-    t.string "base_locale", limit: 255
-    t.string "target_locale", limit: 255
-    t.string "target_dialect", limit: 255
+    t.string "base_locale"
+    t.string "target_locale"
+    t.string "target_dialect"
     t.boolean "is_current_player"
     t.datetime "created_at", precision: nil
     t.datetime "updated_at", precision: nil
-    t.string "avatar_file_name", limit: 255
-    t.string "avatar_content_type", limit: 255
-    t.integer "avatar_file_size"
-    t.datetime "avatar_updated_at", precision: nil
   end
 
   create_table "quantities", id: :serial, force: :cascade do |t|
-    t.string "name", limit: 255
+    t.string "name"
     t.float "value"
   end
 
   create_table "reward_types", id: :serial, force: :cascade do |t|
-    t.string "name", limit: 255
+    t.string "name"
     t.datetime "created_at", precision: nil
     t.datetime "updated_at", precision: nil
   end
@@ -213,7 +210,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_26_180602) do
   end
 
   create_table "search_suggestions", id: :serial, force: :cascade do |t|
-    t.string "term", limit: 255
+    t.string "term"
     t.integer "popularity"
     t.datetime "created_at", precision: nil
     t.datetime "updated_at", precision: nil
@@ -229,20 +226,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_26_180602) do
   end
 
   create_table "users", id: :serial, force: :cascade do |t|
-    t.string "first_name", limit: 255
-    t.string "last_name", limit: 255
+    t.string "first_name"
+    t.string "last_name"
     t.datetime "created_at", precision: nil
     t.datetime "updated_at", precision: nil
-    t.string "email", limit: 255, default: "", null: false
-    t.string "encrypted_password", limit: 255, default: "", null: false
-    t.string "invitation_token", limit: 255
-    t.datetime "invitation_created_at", precision: nil
-    t.datetime "invitation_sent_at", precision: nil
-    t.datetime "invitation_accepted_at", precision: nil
-    t.integer "invitation_limit"
-    t.integer "invited_by_id"
-    t.string "invited_by_type", limit: 255
-    t.string "reset_password_token", limit: 255
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
     t.datetime "reset_password_sent_at", precision: nil
     t.datetime "remember_created_at", precision: nil
     t.integer "sign_in_count", default: 0, null: false
@@ -250,8 +240,17 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_26_180602) do
     t.datetime "last_sign_in_at", precision: nil
     t.inet "current_sign_in_ip"
     t.inet "last_sign_in_ip"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at", precision: nil
+    t.datetime "confirmation_sent_at", precision: nil
+    t.string "unconfirmed_email"
+    t.integer "failed_attempts", default: 0, null: false
+    t.string "unlock_token"
+    t.datetime "locked_at", precision: nil
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
