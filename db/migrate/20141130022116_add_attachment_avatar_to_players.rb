9eac6ebfca7fba483854f3eb363dd51c92d96fee
Fred Willmore <fredwillmore@Freds-MacBook-Pro.local>
2023-09-25 19:18:01 -0700

    commented out obselete paperclip attachments

--- db/migrate/20141130022116_add_attachment_avatar_to_players.rb
+++ db/migrate/20141130022116_add_attachment_avatar_to_players.rb
@@ -1,11 +1,11 @@
 class AddAttachmentAvatarToPlayers < ActiveRecord::Migration[4.2]
   def self.up
-    change_table :players do |t|
-      t.attachment :avatar
-    end
+    # change_table :players do |t|
+    #   t.attachment :avatar
+    # end
   end
 
   def self.down
-    drop_attached_file :players, :avatar
+    # drop_attached_file :players, :avatar
   end
 end
